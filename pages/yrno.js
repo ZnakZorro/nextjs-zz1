3e84864db45c09868a4e273895e795bf1073bf38
ZnakZorro <znakzorro@gmail.com>
2021-05-22 15:10:45 +0200

    1 file modified

--- pages/yrno.js
+++ pages/yrno.js
@@ -2,17 +2,23 @@ function Blog({ posts }) {
   let temp0 = posts.longIntervals[0].temperature.value;
   let temp1 = posts.longIntervals[1].temperature.value;
   let temp2 = posts.longIntervals[1].temperature.value;
-  return <div>
-    <p>Teraz0 {temp0}&deg;C</p>
-    <p>Teraz1 {temp1}&deg;C</p>
-    <p>Teraz2 {temp2}&deg;C</p>
-    
+  return 
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <p>Teraz0 {temp0}&deg;C</p>
+        <p>Teraz1 {temp1}&deg;C</p>
+        <p>Teraz2 {temp2}&deg;C</p>
+      </div>
     </div>
+    
+  </div>
 }
 
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
+  //let url ='https://www.yr.no/api/v0/locations/5-1220500/forecast'; // Szczecin 
   let url ='https://www.yr.no/api/v0/locations/2-3083828/forecast'; // Dąbie
   const res = await fetch(url)
   const posts = await res.json()
