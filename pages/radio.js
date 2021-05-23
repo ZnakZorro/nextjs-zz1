3dab8cc1a2fe1535b76d8e36e5b1df43b866e581
ZnakZorro <znakzorro@gmail.com>
2021-05-23 06:05:36 +0200

    2 files modified

--- pages/radio.js
+++ pages/radio.js
@@ -8,9 +8,11 @@ function Radio() {
     <a href="/"><h1>Radio</h1></a>
     <div class="row">
       <div class="col-md-12">
-        <button onClick="radio(0)" class="btn btn-info">Radio 0</button>
-        <button onClick="radio(1)" class="btn btn-info">Radio 1</button>
-        <button onClick="radio(2)" class="btn btn-info">Radio 2</button>
+        <div className={styles.btn-group}>
+          <button type="button" onClick="radio(0)" className={styles.btn}>Radio 0</button> 
+          <button type="button" onClick="radio(1)" className={styles.btn}>Radio 1</button>
+          <button type="button" onClick="radio(2)" className={styles.btn}>Radio 2</button>
+        </div>
       </div>
     </div>
   </div>
